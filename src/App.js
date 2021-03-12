cee731727e36ee40ad84739f36bb16795c89fe22
LucasSaller <sallerlucas@gmail.com>
2021-03-12 16:52:42 -0300

    Change evaluate from mathjs to simple eval javascript

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {Button} from "./components/Button";
 import {Input} from "./components/Input";
 import {ClearButton} from "./components/ClearButton";
-import * as math from 'mathjs';
+
 
 class App extends Component{
   constructor(props){
@@ -18,7 +18,7 @@ class App extends Component{
   };
 
   handleEqual = () => {
-    this.setState({ input:math.evaluate(this.state.input) })
+    this.setState({ input:eval(this.state.input) })
   }
 
   render(){
